365308de0fb3198070417a0c6838446c69effdab
hyeon-i <soohoioa@gmail.com>
2024-06-15 14:43:24 +0900

    6.15 스프링 부트 프로젝트 시작

---
+++ src/main/java/com/fc/projectboard/FaProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fc.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaProjectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fc/projectboard/FaProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
